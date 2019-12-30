5eb45f15286e29a6f1746151b97c378b1c73ea6f
howardjhs7 <howardjhs7@cm.nsysu.edu.tw>
2019-12-30 21:30:28 +0800

    add data.js

---
+++ data.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "",
+    authDomain: "whattoeat-mis.firebaseapp.com",
+    databaseURL: "https://whattoeat-mis.firebaseio.com",
+    projectId: "whattoeat-mis",
+    storageBucket: "whattoeat-mis.appspot.com",
+    messagingSenderId: "101696256042",
+    appId: "1:101696256042:web:f40e2d85f791e483bd2b53"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.database();
